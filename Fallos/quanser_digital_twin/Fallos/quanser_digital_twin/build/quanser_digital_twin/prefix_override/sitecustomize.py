1cede0454cca47a6be7c4341e8b2611a268afbdd
luisporras20 <144760314+luisporras20@users.noreply.github.com>
2025-10-31 09:03:30 -0500

    Delete Fallos/quanser_digital_twin directory

--- Fallos/quanser_digital_twin/Fallos/quanser_digital_twin/build/quanser_digital_twin/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/investigacion/ros2_ws/src/quanser_digital_twin/install/quanser_digital_twin'
--- Fallos/quanser_digital_twin/Seguridad_Funcional/prototipo_v7_final/prototipo_v7/src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/centelsa')
-.then(db => console.log('DB is connected'))
-.catch(err => console.log(err));
